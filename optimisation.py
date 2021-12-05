af76a3521bc5c18280da482299631c44352bf631
unknown <almehdi.krisni@gmail.com>
2021-12-05 19:24:09 +0100

    Gurobi optimization done

--- optimisation.py
+++ optimisation.py
@@ -68,32 +68,16 @@ def optPlusCourtChemin(graphe ,start, end) :
     b += [1]
     a += [cont2]
 
-    # Contrainte 3 - Il existe au plus une seule arête sortante depuis un sommet s
-    # Il s'agit d'une contrainte de forme A <= B
+    # PROTOTYPE
     cont3 = []
 
     for i in range(nbSommets) :
-        tempC = [link[i][j] for j in range(nbSommets)] # Liste des arcs sortants du sommet i (soit le sommet s) vers les sommets j
+        tempC = [link[i][j] for j in range(nbSommets)] + [link[j][i] for j in range(nbSommets)]
         cont3.append(tempC)
-        b += [1] # On met à jour le second membre
+        b += [0] # On met à jour le second membre
 
     # On met à jour la matrice des contraintes et la matrice des seconds membres
     a += cont3
-
-
-
-    # Contrainte 4 - Il existe au plus une seule arête entrante vers un sommet s
-    # Il s'agit d'une contrainte de forme A <= B
-    cont4 = []
-
-    for j in range(nbSommets) :
-        tempC = [link[i][j] for i in range(nbSommets)] # Liste des arcs sortants du sommet i vers le sommet j (soit le sommet s)
-        cont4.append(tempC)
-        b += [1]
-
-    # On met à jour la matrice des contraintes et la matrice des seconds membres
-    a += cont4
-
 
 
     # On crée le modèle
@@ -118,7 +102,7 @@ def optPlusCourtChemin(graphe ,start, end) :
         obj += c[i] * x[i] # La fonction objectif ne prend donc en compte que les arcs existants dans le graphe (grâce aux coefficients)
 
     # On définit le choix d'optimisation de l'objectif (MAXIMIZE ou MINIMIZE)
-    m.setObjective(obj, GRB.MAXIMIZE)
+    m.setObjective(obj, GRB.MINIMIZE)
 
     print("\n\n\n")
 
@@ -129,49 +113,21 @@ def optPlusCourtChemin(graphe ,start, end) :
     for i in range(len(b)) : # len(b) = nombre de contraintes
         if (i < 2) : # Contraintes à l'égalite
             if (i == 0) : # Contrainte 1
-                m.addConstr(quicksum(a[i][j] * x[index[start] * (nbSommets - 1) + j] for j in range(nbSommets)) == b[i], "Contrainte " + str(i))
+                m.addConstr(quicksum(a[i][j] * x[index[start] * (nbSommets) + j] for j in range(nbSommets)) == b[i], "Contrainte " + str(i))
 
             else : # Contrainte 2
                 m.addConstr(quicksum(a[i][j] * x[index[end] + (nbSommets) * j] for j in range(nbSommets)) == b[i], "Contrainte " + str(i))
 
-        else : # Contraintes à l'infériorité
-            # 
-            if (i < len(b) - nbSommets) : # Contrainte 3
-                k = (i - 2) % nbSommets
-                m.addConstr(quicksum(a[i][j] * x[int(k * (nbSommets) + j)] for j in range(nbSommets)) <= b[i], "Contrainte " + str(i))
-
-                # print([a[i][j] * x[int(k * (nbSommets) + j)] for j in range(nbSommets)])
-            
-            else : # Contrainte 4
-                k = (i - 2) % nbSommets
-                m.addConstr(quicksum(a[i][j] * x[int(j * (nbSommets) + k)] for j in range(nbSommets)) <= b[i], "Contrainte " + str(i))
-
-                print([a[i][j] * x[int(j * (nbSommets) + k)] for j in range(nbSommets)])
-
-
-    print("\n\n\n")
+        else :
+            if (i - 2 not in [index[start], index[end]]) : # On ne prend en compte que si il ne s'agit pas du noeud d'entrée ou de noeud de sortie
+                k = (i - 2) % nbSommets # On récupère l'indice du sommet étudié
+                m.addConstr(quicksum(a[i][j] * x[int(k * (nbSommets) + j)] for j in range(nbSommets)) - quicksum(a[i][j + nbSommets] * x[int(j * (nbSommets) + k)] for j in range(nbSommets)) == b[i], "Contrainte " + str(i))
 
     # On effectue la résolution
     m.optimize()
 
-    print([(v.varName, v.X) for v in m.getVars() if abs(v.obj) > 1e-6])
-
-    if m.status == GRB.OPTIMAL:
-        print()
-        print('Optimal objective: %g' % m.objVal)
-        print()
-    if m.status == GRB.INF_OR_UNBD:
-        print('Model is infeasible or unbounded')
-        sys.exit(0)
-    if m.status == GRB.INFEASIBLE:
-        print('Model is infeasible')
-        sys.exit(0)
-    if m.status == GRB.UNBOUNDED:
-        print('Model is unbounded')
-        sys.exit(0)
-    else:
-        print('Optimization ended with status %d' % m.status)
-        sys.exit(0)
+    # On affiche la solution optimale
+    print("Chemin le plus court trouvé :", [(v.varName, v.X) for v in m.getVars() if abs(v.X) > 1e-6])
 
 #######################################################################################################
 # PARTIE TEST
